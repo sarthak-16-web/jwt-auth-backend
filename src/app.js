301fb5745f179f96b0a43727026da6556ed89052
Sarthak Kasat <kasat4309@gmail.com>
2026-03-20 16:55:05 +0530

    Update CORS configuration for production

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ import express from 'express' ;
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
 const app = express();
+import cors from "cors";
 
-app.use(cors({
-    origin:"http://localhost:5173" ,
-    credentials : true
-}));
+app.use(
+  cors({
+    origin: "https://jwt-auth-frontend-16.onrender.com",
+    credentials: true,
+  })
+);
 
 app.use(express.json({limit:"10mb"}))
 app.use(express.urlencoded({extended : true , limit:"10mb" }))
